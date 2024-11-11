1f3671fe009ed51e97f5810aa874d7794029a5be
Lucas Klitzke <lucas_eduardoksilva@hotmail.com>
2024-11-11 20:23:45 -0300

    Fix: Atualizando projeto para .NET 8 e organizando diretório

--- DesafioProjetoHospedagem/Program.cs
+++ DesafioProjetoHospedagem/Program.cs
@@ -2,6 +2,8 @@
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("-------- Iniciando programa: Hospedagem em Hotel --------\n");
+Console.ReadKey();
 
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
@@ -16,10 +18,12 @@ hospedes.Add(p2);
 Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 20);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.ReadKey();
+Console.WriteLine("Finalizando programa...");
